868510caf81d7eb89d3bdf13f1c8e4fa6e0be1f8
Daniel Silva <daniel@Daniels-MacBook-Air.local>
2022-02-11 11:02:03 -0600

    automated test done

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -17,4 +17,16 @@ test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
+    // await driver.sleep(3000)
+})
+
+test('can you draw the bots', async () => {
+    const draw = await driver.findElement(By.css('button#draw'))
+    await draw.click()
+    await driver.sleep(2000)
+} )
+test('can you see all bots', async () => {
+    const add = await driver.findElement(By.css('button#see-all'))
+    await add.click()
+    await driver.sleep(3000)
 })
